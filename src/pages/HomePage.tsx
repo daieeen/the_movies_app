c5a575dd6424e830c784243f3c052bc7f296fe5f
daieeen <101250985+daieeen@users.noreply.github.com>
2022-05-10 14:48:52 +0900

    modd

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { CategoryButton } from '../components/CategoryButton';
-import { MovieCard } from '../components/MovieCard';
-import popularImg from '../assets/images/popcorn.png'
-import nowPlayingImg from '../assets/images/seats.jpeg'
+import React, { useEffect, useState } from "react";
+import { CategoryButton } from "../components/CategoryButton";
+import { MovieCard } from "../components/MovieCard";
+import popularImg from "../assets/images/popcorn.png";
+import nowPlayingImg from "../assets/images/seats.jpeg";
+import upcomingImg from "../assets/images/Movieimg.png";
 
 const API_KEY = "c275787762fb2904adb52c4ad6412662";
-
-export type Category ={
+export type Category = {
   id: number;
   label: string;
   url: string;
@@ -14,8 +14,9 @@ export type Category ={
 };
 
 const CATEGORY_LIST = [
-{id: 0, label:'인기영화', url:'/popular', image: popularImg},
-{id: 1, label:'현재 상영작', url:'/now_playing', image: nowPlayingImg},
+  { id: 0, label: "인기영화", url: "/popular", image: popularImg },
+  { id: 1, label: "현재 상영작", url: "/now_playing", image: nowPlayingImg },
+  { id: 2, label: "최신 영화", url: "/upcoming", image:upcomingImg },
 ];
 
 export type Movie = {
@@ -24,18 +25,18 @@ export type Movie = {
   overview: string;
   poster_path: string;
   vote_average: number;
-}
+};
 
 export const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [movies, setMovies] = useState<Movie[]>([]);
   const [categoryIndex, setCategoryIndex] = useState(0);
 
-  const setCategory = (index:number) => {
+  const setCategory = (index: number) => {
     setCategoryIndex(index);
-  }
+  };
 
-  const getData = async (categoryIndex:number) => {
+  const getData = async (categoryIndex: number) => {
     const url = `https://api.themoviedb.org/3/movie${CATEGORY_LIST[categoryIndex].url}?api_key=${API_KEY}&language=ko-KR&page=1`;
     const response = await fetch(url);
     if (response.status === 200) {
@@ -46,7 +47,7 @@ export const HomePage = () => {
       throw new Error("데이터를 받아오지 못했습니다.");
     }
     setIsLoading(false);
-  }
+  };
 
   useEffect(() => {
     getData(categoryIndex);
@@ -69,13 +70,20 @@ export const HomePage = () => {
         <div className="space-y-4">
           <div className="text-2xl font-bold">Category</div>
 
-        <div className="flex space-x-3">
-          {CATEGORY_LIST.map((data) => <CategoryButton key={data.id} category={data} onClick={setCategory} isSelected={data.id === categoryIndex}/>)}
+          <div className="flex space-x-3">
+            {CATEGORY_LIST.map((data) => (
+              <CategoryButton
+                key={data.id}
+                category={data}
+                onClick={setCategory}
+                isSelected={data.id === categoryIndex}
+              />
+            ))}
           </div>
         </div>
       </div>
 
-{/* <div>
+      {/* <div>
     <div className="text-2xl font-bold mb-4">List</div>  
       <div className="border p-4 rounded-mb">
         <div>
@@ -100,32 +108,33 @@ export const HomePage = () => {
 
 </div> */}
       <div className="text-2xl font-bold mb-4">Today's Special</div>
-    
-     {!isLoading && movies.map((movie) =>(
-        <MovieCard key={movie.id} movie={movie} />
-        ))}
+
+      {!isLoading &&
+        movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
 
       <div className="flex space-x-3">
         <div>
-          <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60" 
-          alt=""
-          className="w-full h-30 object-cover rounded-xl"
+          <img
+            src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"
+            alt=""
+            className="w-full h-30 object-cover rounded-xl"
           />
           <div className="mt-4">
             <div className="text-lg font-semibold">52번가 샐러드</div>
-            <div className="text-sm text-gray-500"> 
-            서울 서대문구 이화여대길 52-15
             <div className="text-sm text-gray-500">
-            02-1234-5678
-            <div className="text-sm text-gray-500">
-              Mon-SAT 9:00 AM - 9:00 PM 
+              서울 서대문구 이화여대길 52-15
+              <div className="text-sm text-gray-500">
+                02-1234-5678
+                <div className="text-sm text-gray-500">
+                  Mon-SAT 9:00 AM - 9:00 PM
+                </div>
+                <div className="m-4">
+                  <div className="bg-gray-800 rounded-md text-white text-center py-2.5 px-4">
+                    네이버 지도로 길찾기
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="m-4">
-            <div className="bg-gray-800 rounded-md text-white text-center py-2.5 px-4">네이버 지도로 길찾기</div>
-            </div>
-            </div>
-            </div>
-
           </div>
         </div>
       </div>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -53,3 +53,4 @@ export const LoginPage = () => {
       </div>
   );
 };
+
